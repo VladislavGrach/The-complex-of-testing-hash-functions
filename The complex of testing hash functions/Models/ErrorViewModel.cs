60e0bafdea22a05f0c39ed7b2863de73cbd53259
VladislavGrach <vladislav.grachev.03@mail.ru>
2025-03-06 22:10:37 +0300

    Добавьте файлы проекта.

---
+++ The complex of testing hash functions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace The_complex_of_testing_hash_functions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
